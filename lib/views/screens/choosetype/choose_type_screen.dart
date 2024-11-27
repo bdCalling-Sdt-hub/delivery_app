bd9d48d909c5b6123f7f4195d11a2009c4e81b21
Your Name <ebrahim.cse.bu@gmail.com>
2024-11-27 12:13:37 +0600

    test

--- lib/views/screens/choosetype/choose_type_screen.dart
+++ lib/views/screens/choosetype/choose_type_screen.dart
@@ -55,7 +55,7 @@ class _ChooseTypeScreenState extends State<ChooseTypeScreen> {
               ),
             ],
           ),
-          SizedBox(height: 50.h),
+          SizedBox(height: 52.h),
           Container(
             decoration: BoxDecoration(
                 borderRadius:
